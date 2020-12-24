dd2a6417f8e1031a0dc47f234630f73d94161620
danilbakshaev <bakshaevdanil@gmail.com>
2020-12-24 16:05:30 +0500

    Переписал без jQuery

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
-$(document).ready(function () {
-  let position = 0
-  const slidesToShow = 4;
-  const slidesToScroll = 3;
-  const container = $('.slider-container');
-  const track = $('.slider-track');
-  const item = $('.slider-item');
-  const btnPrev = $('.btn-prev');
-  const btnNext = $('.btn-next');
-  const itemsCount = item.length;
-  const itemWidth = container.width() / slidesToShow;
-  const movePosition = slidesToScroll * itemWidth;
 
-  item.each(function (index, item) {
-    $(item).css ({
-      minWidth: itemWidth,
-    })    
-  })
+let position = 0
+const slidesToShow = 3;
+const slidesToScroll =2;
+const container = document.querySelector('.slider-container')
+const track = document.querySelector('.slider-track')
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
+const items = document.querySelectorAll('.slider-item');
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
 
-  btnNext.click(function(){
-    const itemLeft = itemsCount - (Math.abs (position) + slidesToShow * itemWidth) / itemWidth;
+items.forEach(item => {
+  item.style.minWidth = `${itemWidth}px`
+});
 
-    position -= itemLeft >= slidesToShow ? movePosition : itemLeft * itemWidth;
-    setPosition();
-    checkBtns();
-  })
+btnNext.addEventListener('click', () =>{
+  const itemLeft = itemsCount - (Math.abs (position) + slidesToShow * itemWidth) / itemWidth;
 
-  btnPrev.click(function(){
-    const itemLeft = Math.abs (position) / itemWidth;
+  position -= itemLeft >= slidesToShow ? movePosition : itemLeft * itemWidth;
+  setPosition();
+  checkBtns();
+});
 
-    position += itemLeft >= slidesToShow ? movePosition : itemLeft * itemWidth;
-    setPosition();
-    checkBtns();
-  })
+btnPrev.addEventListener('click', () =>{
+  const itemLeft = Math.abs (position) / itemWidth;
 
-  const setPosition = () => {
-    track.css({
-      transform: `translateX(${position}px)`
-    })
-  }
+  position += itemLeft >= slidesToShow ? movePosition : itemLeft * itemWidth;
+  setPosition();
+  checkBtns();
+});
 
-  const checkBtns = () => {
-    btnPrev.prop('disabled', position === 0);
-    btnNext.prop(
-      'disabled',
-      position <= -(itemsCount - slidesToShow)*itemWidth
-    );
-  }
+const setPosition = () => {
+  track.style.transform = `translateX(${position}px)`
+};
 
-  checkBtns()
+const checkBtns = () => {
+  btnPrev.disabled = position === 0;
+  btnNext.disabled = position <= -(itemsCount - slidesToShow)*itemWidth;
+};
 
-})
+checkBtns();
+
